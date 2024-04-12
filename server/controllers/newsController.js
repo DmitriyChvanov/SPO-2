b1db8b89feaf933a061014912714570d4b051927
JustAdustie <58479779+JustAdustie@users.noreply.github.com>
2024-04-12 22:26:19 +0300

    title
    123

--- server/controllers/newsController.js
+++ server/controllers/newsController.js
@@ -6,12 +6,12 @@ const ApiError = require("../error/apiError")
 class newsController {
     async create(req,res,next){
         try{
-            const {description} = req.body
+            const {description, title} = req.body
             const {img} = req.files
             let fileName = uuid.v4() + ".jpg"
             img.mv(path.resolve(__dirname,"..", "static", fileName))
 
-            const news_item = await News_item.create({description, img: fileName})
+            const news_item = await News_item.create({description, title, img: fileName})
 
             return res.json(news_item)
         } catch(e){
